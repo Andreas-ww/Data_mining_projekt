01f1f2ef7e5cd755b313a33f8a98e39cc95e131f
Andreas Westroth <andreas.westroth@gmail.com>
2020-10-07 17:45:59 +0200

    Ändra lite grejer

--- read_data.R
+++ read_data.R
@@ -3,33 +3,39 @@
 #Läser in data
 data <- read.csv("letter-recognition.data" , header = F)
 
+colnames(data) <- c("Y", "x-box" , "y-box" , "width", "high", "onpix",
+                    "x-bar" , "y-bar","x2bar","y2bar","xybar","x2ybr",
+                    "xy2br", "x-ege","xegvy","y-ege","yegvx")
 
-###förvandlar bokstavär till int
 
-data$"Y" <- 0
+###förvandlar bokstavär till int // Behövs nog inte
 
-for(i in 1:length(letters)){
+# data$"Y" <- 0
+# 
+# for(i in 1:length(letters)){
+# 
+#   ind <- data$V1==LETTERS[i]
+#  data$Y[ind] <- i-1
+#  }
 
-  ind <- data$V1==LETTERS[i]
-  data$Y[ind] <- i-1
-}
-
-#Skapar training / validation set
+#Skapar training / test set
 set.seed(1234)
 
-train_index <- createDataPartition(data$V1, p=0.7 , list=F)
+#train_index <- createDataPartition(data$Y, p=0.8 , list=F)
+
+train_index<- sample(1:nrow(data) , 16000)
 
 train_data <- data[train_index,]
 
-val_data <- data[-train_index,]
+test_data <- data[-train_index,]
 
 
 #Delar upp Responsvariabel och förklarande variabler
-train_y <- train_data$V1
-val_y <- val_data$V1
-
-train_x <- as.matrix(train_data[,-1])
-val_x <- as.matrix(val_data[,-1])
+# train_y <- train_data$V1
+# val_y <- val_data$V1
+# 
+# train_x <- as.matrix(train_data[,-1])
+# val_x <- as.matrix(val_data[,-1])
 
 
 
